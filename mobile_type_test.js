0003b5c5e5017053fef47ad842905c3076288a1a
ChengWenxin <1260077156@qq.com>
2018-10-14 17:00:48 +0800

    task #1

---
+++ mobile_type_test.js
@@ -0,0 +1,14 @@
+var ua   = navigator.userAgent.toLowerCase();   
+if(ua.match(/android/i) === "android")  
+{  
+    alert("android");  
+}  
+if(ua.match(/iphone/i) === "iPhone")  
+{  
+    alert("iPhone");  
+}  
+if(ua.match(/ipad/i) === "iPad")  
+{  
+    alert("ipad");  
+}  
+console.log("what???")
